2c4e2d8a3acf11b33f1e376706947612a0b962dc
Bram <bram@fluet.dev>
2024-11-20 19:46:50 +0100

    working base

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.watertall {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens com.example.watertall to javafx.fxml;
     exports com.example.watertall;
