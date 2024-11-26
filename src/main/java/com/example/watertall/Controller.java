e49a417c2cfef71af170368390811a7db12f41d3
Bram <bram@fluet.dev>
2024-11-26 22:03:34 +0100

    added get location

--- src/main/java/com/example/watertall/Controller.java
+++ src/main/java/com/example/watertall/Controller.java
@@ -12,6 +12,16 @@ import javafx.util.Duration;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Controller implements Initializable
 {
 
@@ -26,6 +36,9 @@ public class Controller implements Initializable
 
     @FXML
     private AnchorPane slider;
+
+    @FXML
+    private Label locationLabel;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
@@ -72,5 +85,70 @@ public class Controller implements Initializable
                 MenuClose.setVisible(false);
             });
         });
+    }
+    public void getLocation()
+    {
+        new Thread(() ->
+        {
+            try
+            {
+                URL url = new URL("http://ip-api.com/json/");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null)
+                {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                String location = parseLocation(jsonResponse);
+
+                Platform.runLater(() ->
+                {
+                    if (location != null)
+                    {
+                        locationLabel.setText("Location: " + location);
+                    } else
+                    {
+                        showError("Unable to determine location.");
+                    }
+                });
+            } catch (Exception e)
+            {
+                Platform.runLater(() -> showError("An error occurred while fetching location."));
+            }
+        }).start();
+    }
+
+    private String parseLocation(String json)
+    {
+        try
+        {
+            if (json.contains("\"city\":\"") && json.contains("\"regionName\":\""))
+            {
+                String city = json.split("\"city\":\"")[1].split("\"")[0];
+                String region = json.split("\"regionName\":\"")[1].split("\"")[0];
+                return city + ", " + region;
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void showError(String message)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
